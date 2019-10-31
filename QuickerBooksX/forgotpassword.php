7f9e040d4dbf8f2bc6200818b5497872627b29b6
jessicamuh <49791345+jessicamuh@users.noreply.github.com>
2019-10-31 16:06:09 -0400

    Update forgotpassword.php

--- QuickerBooksX/forgotpassword.php
+++ QuickerBooksX/forgotpassword.php
@@ -2,7 +2,8 @@
 include('config.php');
 session_start();
 
-	if(!empty($_POST["forgot-password"])){
+if (isset($_POST['forgot_password'])) {
+
 		$conn = mysqli_connect("localhost", "root", "", "blog_samples");
 		
 		$condition = "";
@@ -22,7 +23,6 @@ session_start();
 		$sql = "Select * from members " . $condition;
 		$result = mysqli_query($conn,$sql);
 		$user = mysqli_fetch_array($result);
-		
 		if(!empty($user)) {
 			require_once("forgot-password-recovery-mail.php");
 		} else {
@@ -35,7 +35,7 @@ session_start();
 integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <form name="frmForgot" id="frmForgot" method="post" onSubmit="return validate_forgot();">
 
-<h4>Forgot Password?</h4>
+<h3>Forgot Password?</h3>
 	<?php if(!empty($success_message)) { ?>
 	<div class="success_message"><?php echo $success_message; ?></div>
 	<?php } ?>
@@ -44,19 +44,32 @@ integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw
 		<?php if(!empty($error_message)) { ?>
 	<?php echo $error_message; ?>
 	<?php } ?>
-	</div>
-
-	<div class="field-group">
-		<div><label for="username">Username</label></div>
-		<div><input type="text" name="user-login-name" id="user-login-name" class="input-field"> Or</div>
-	</div>
-	
+	</div>	
 	<div class="field-group">
 		<div><label for="email">Email</label></div>
 		<div><input type="text" name="user-email" id="user-email" class="input-field"></div>
 	</div>
-	
 	<div class="field-group">
 		<div><input type="submit" name="forgot-password" id="forgot-password" value="Submit" class="form-submit-button"></div>
 	</div>	
 </form>
+
+<h4>Please answer the following security questions!!</h4>
+      <form id="securityform" onsubmit="return false;">
+        <div>Question 1:</div>
+        <p><?php echo $q1 = "Security Question 1"; ?></p>
+        <input name="ans1" id="ans1" type="text" onfocus="_('status').innerHTML='';" maxlength="100">
+        <br/><br/>
+        <div>Question 2:</div>
+        <p><?php echo $q2 = "Security Question 2"; ?></p>
+        <input name="ans2" id="ans2" type="text" onfocus="_('status').innerHTML='';" maxlength="100">
+        <br /><br />
+        <input name="email" id="email" type="hidden" value="<?php echo $email; ?>" />
+
+        <button id="anssubmitbtn" onclick="forgotpasscon()">Submit</button> 
+        <p id="status"></p>
+      </form>
+    </div>
+    <?php include_once("template_pageBottom.php"); ?>
+    </body> 
+    </html> 
